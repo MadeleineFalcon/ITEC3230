4c122e8777f61845d030a5325a2b8e7a931a5cee
Madeleine <madeleinefalcon17@gmail.com>
2025-11-23 19:36:21 -0500

    set up files, create nav, add global styles

--- script.js
+++ script.js
@@ -1,29 +1,11 @@
 
 
 /////////////////////////////////////////////////UPDATE NAV ON LOGIN
-function updateNavigation() {
-  if (sessionStorage.getItem('isLoggedIn') === 'true') {
-    const authButton = document.getElementById('authButton');
-    const signIn = document.getElementById('signIn')
-    if (signIn) {
-      signIn.innerHTML = '<a href="profile.html">Profile</a>';
-    }
-    if (book) {
-      book.innerHTML = '<a href="book.html">Book</a>';
-    }
-  }
-}
 
-function handleLogin(event) {
-  event.preventDefault();
-  
-  const username = document.getElementById('username').value;
-  sessionStorage.setItem('isLoggedIn', 'true');
-  window.location.href = 'home.html';
-  
-  return false;
-}
 
-document.addEventListener('DOMContentLoaded', updateNavigation);
+//TO DO:
+//add js so once user logs in or registers the log in button changes to profile button
+
+
 
 ////////////////////////////////////////////////////////////////////
